9fe9d7e990182ed7a469fc1f44df1d77cb5dbb83
Adam Helton <helton93@protonmail.com>
2019-11-11 13:18:04 -0500

    added sample app routes

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+wsgi_app = app.wsgi_app
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+    
