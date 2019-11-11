4f98914c95890e9334064de4cb08e37cbc0fbe36
Adam Helton <helton93@protonmail.com>
2019-11-11 13:23:29 -0500

    added os environment and server host

--- app.py
+++ app.py
@@ -12,3 +12,12 @@ def home():
 def about():
     return render_template("about.html")
     
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
